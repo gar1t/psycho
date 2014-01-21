6e211d95ec76988cd536078b8b13e3457aef30e3
Garrett Smith <g@rre.tt>
2014-01-20 20:51:08 -0600

    Burgeoning tests

---
+++ src/psycho_tests.erl
@@ -0,0 +1,85 @@
+-module(psycho_tests).
+
+-export([run/0]).
+
+run() ->
+    test_parse_request_path(),
+    test_ensure_parsed_request_path(),
+    test_routes().
+
+test_parse_request_path() ->
+    io:format("parse_request_path: "),
+    P = fun(S) -> psycho_util:parse_request_path(S) end,
+
+    %% Empty case
+    {"", "", []} = P(""),
+
+    %% Simple paths
+    {"/foo", "", []} = P("/foo"),
+    {"/foo/bar", "", []} = P("/foo/bar"),
+
+    %% Path with simple query strings
+    {"/foo", "bar=123", [{"bar", "123"}]} =
+        P("/foo?bar=123"),
+    {"/foo", "bar=123&baz=456", [{"bar", "123"}, {"baz", "456"}]} =
+        P("/foo?bar=123&baz=456"),
+
+    %% Query strings with names only
+    {"/foo", "bar=123&baz", [{"bar", "123"}, {"baz", ""}]} =
+        P("/foo?bar=123&baz"),
+    {"/foo", "baz&bar=123", [{"baz", ""}, {"bar", "123"}]} =
+        P("/foo?baz&bar=123"),
+
+    %% Query strings with multiple values
+    {"/foo", "bar=123&bar=456", [{"bar", "123"}, {"bar", "456"}]} =
+        P("/foo?bar=123&bar=456"),
+
+    io:format("OK~n").
+
+test_ensure_parsed_request_path() ->
+    io:format("ensure_parsed_request_path: "),
+    E = fun(Env) -> psycho_util:ensure_parsed_request_path(Env) end,
+
+    Path = "/foo?bar=123&bar=456",
+    Parsed = psycho_util:parse_request_path(Path),
+    Env0 = [{request_path, Path}],
+
+    {Parsed, Env1} = E(Env0),
+    Env1 = [{parsed_request_path, Parsed}|Env0],
+    {Parsed, Env1} = E(Env1),
+
+    io:format("OK~n").
+
+test_routes() ->
+    io:format("routes: "),
+    R = fun(Routes, Env) -> psycho_route:route(Routes, Env) end,
+    R2 = fun(Routes, Env, Opts) -> psycho_route:route(Routes, Env, Opts) end,
+    App = fun(Result) -> fun(_Env) -> Result end end,
+    Env = fun(Path) -> [{request_path, Path}] end,
+
+    NotFoundHandlerOpts = [{not_found_handler, App(not_found)}],
+
+    Routes =
+        [{"/", App(root)},
+         {"/foo", App(foo)},
+         {{exact, "/bar"}, App(bar)},
+         {{starts_with, "/bar"}, App(starts_with_bar)},
+         {{matches, "^/baz/(bam|BAM)$"}, App(baz_bam)},
+         {{matches, "^/baz/"}, App(baz_other)}],
+
+    %% Test routes to app proxies
+    root = R(Env("/"), Routes),
+    foo = R(Env("/foo"), Routes),
+    bar = R(Env("/bar"), Routes),
+    starts_with_bar = R(Env("/bar/baz"), Routes),
+    baz_bam = R(Env("/baz/bam"), Routes),
+    baz_bam = R(Env("/baz/BAM"), Routes),
+    baz_other = R(Env("/baz/bAm"), Routes),
+    baz_other = R(Env("/baz/bam/foo"), Routes),
+    not_found = R2(Env("baz/bam"), Routes, NotFoundHandlerOpts),
+    not_found = R2(Env("/not_handled"), Routes, NotFoundHandlerOpts),
+
+    %% Default not found handler
+    {{404, "Not Found"}, _, _} = R(Env("/not_handled"), Routes),
+
+    io:format("OK~n").
