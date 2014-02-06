c36d83a0b19916252ece0506dbe43d30e4470868
Garrett Smith <g@rre.tt>
2014-02-06 03:28:40 -0600

    Dispatch to modules by path

--- src/psycho_route.erl
+++ src/psycho_route.erl
@@ -1,6 +1,12 @@
 -module(psycho_route).
 
--export([create_app/1, create_app/2, route/2, route/3]).
+-export([create_app/1, create_app/2, route/2, route/3,
+         dispatch_app/1, dispatch_app/2,
+         default_not_found_app/0]).
+
+%%%===================================================================
+%%% Route app
+%%%===================================================================
 
 create_app(Routes) ->
     create_app(Routes, []).
@@ -52,5 +58,57 @@ maybe_dispatch(false, _App, Rest, Method, Path, Env, Options) ->
 not_found_handler(Options) ->
     proplists:get_value(not_found_handler, Options, fun default_not_found/1).
 
+%%%===================================================================
+%%% Default page handlers
+%%%===================================================================
+
+default_not_found_app() ->
+    fun(Env) -> default_not_found(Env) end.
+
 default_not_found(_Env) ->
     {{404, "Not Found"}, [{"Content-Type", "text/plain"}], "Not Found"}.
+
+%%%===================================================================
+%%% Dispatch app
+%%%===================================================================
+
+dispatch_app(Spec) ->
+    dispatch_app(Spec, []).
+
+dispatch_app(Spec, Options) ->
+    fun(Env) -> dispatch(app_for_path(Spec, Options, Env), Options) end.
+
+app_for_path({module, Prefix, Suffix}, _Options, Env) ->
+    mod_for_path(Env, Prefix, Suffix).
+
+mod_for_path(Env0, Prefix, Suffix) ->
+    {{Path, _, _}, Env} = psycho_util:ensure_parsed_request_path(Env0),
+    MaybeMod = try_mod_for_parts([Prefix, path_to_mod_part(Path), Suffix]),
+    {MaybeMod, Env}.
+
+path_to_mod_part("/" ++ Path) ->
+    path_to_mod_part(Path);
+path_to_mod_part("") ->
+    "index";
+path_to_mod_part(Path) ->
+    replace(Path, $/, $_, []).
+
+replace([Replace|Rest], Replace, With, Acc) ->
+    replace(Rest, Replace, With, [With|Acc]);
+replace([C|Rest], Replace, With, Acc) ->
+    replace(Rest, Replace, With, [C|Acc]);
+replace([], _Replace, _With, Acc) ->
+    lists:reverse(Acc).
+
+try_mod_for_parts(Parts) ->
+    Flattened = lists:flatten(Parts),
+    try list_to_existing_atom(Flattened) of
+        A -> {ok, A}
+    catch
+        error:badarg -> error
+    end.
+
+dispatch({{ok, App}, Env}, _Options) ->
+    psycho:call_app(App, Env);
+dispatch({error, Env}, Options) ->
+    psycho:call_app(not_found_handler(Options), Env).
