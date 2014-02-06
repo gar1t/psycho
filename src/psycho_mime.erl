65c952dabb7e3b3cca719988f2fc0e4de8f109c1
Garrett Smith <g@rre.tt>
2014-02-06 03:18:33 -0600

    Refactor the way MIME types are initialized
    The previous implementation was an attempt at lazy initialization,
    which of course had a horrible race condition. This implementation
    requires an explict init/0 call, which must be handled by the app.

--- src/psycho_mime.erl
+++ src/psycho_mime.erl
@@ -1,12 +1,49 @@
 -module(psycho_mime).
 
--export([type_from_path/1,
+-export([init/0,
+         type_from_path/1,
          type_from_path/2,
          type_from_extension/1,
          type_from_extension/2]).
 
 -define(TABLE, psycho_mime_types).
 -define(MIME_TYPES_FILE, "mime.types").
+
+init() ->
+    init_table(load_priv_types()).
+
+load_priv_types() ->
+    case file:consult(priv_mime_types()) of
+        {ok, [Types]} -> Types;
+        _ -> []
+    end.
+
+priv_mime_types() ->
+    filename:join(psycho:priv_dir(), ?MIME_TYPES_FILE).
+
+init_table(Types) ->
+    ensure_table(),
+    ets:insert(?TABLE, table_objects(Types)).
+
+ensure_table() ->
+    TableOpts = [named_table, public, set, {read_concurrency, true}],
+    try ets:new(?TABLE, TableOpts) of
+        ?TABLE -> ok
+    catch
+        error:badarg -> ok
+    end.
+
+table_objects(Types) ->
+    acc_table_objects(Types, []).
+
+acc_table_objects([{Type, Extensions}|Rest], Acc) ->
+    acc_table_objects(Type, Extensions, Rest, Acc);
+acc_table_objects([], Acc) -> Acc.
+
+acc_table_objects(Type, [Extension|RestExts], RestTypes, Acc) ->
+    acc_table_objects(Type, RestExts, RestTypes, [{Extension, Type}|Acc]);
+acc_table_objects(_Type, [], RestTypes, Acc) ->
+    acc_table_objects(RestTypes, Acc).
 
 type_from_path(Path) ->
     type_from_path(Path, undefined).
@@ -28,39 +65,5 @@ type_from_extension(Extension, Default) when is_list(Extension) ->
         [{_, Type}] -> Type;
         _ -> Default
     catch
-        error:badarg ->
-            handle_table_init(Extension, Default)
+        error:badarg -> error(not_initialized)
     end.
-
-handle_table_init(RequestedExtension, Default) ->
-    init_table(load_priv_types()),
-    case ets:lookup(?TABLE, RequestedExtension) of
-        [{_, Type}] -> Type;
-        _ -> Default
-    end.
-
-load_priv_types() ->
-    case file:consult(priv_mime_types()) of
-        {ok, [Types]} -> Types;
-        _ -> []
-    end.
-
-priv_mime_types() ->
-    filename:join(psycho:priv_dir(), ?MIME_TYPES_FILE).
-
-init_table(Types) ->
-    TableOpts = [named_table, public, set, {read_concurrency, true}],
-    ets:new(?TABLE, TableOpts),
-    ets:insert(?TABLE, table_objects(Types)).
-
-table_objects(Types) ->
-    acc_table_objects(Types, []).
-
-acc_table_objects([{Type, Extensions}|Rest], Acc) ->
-    acc_table_objects(Type, Extensions, Rest, Acc);
-acc_table_objects([], Acc) -> Acc.
-
-acc_table_objects(Type, [Extension|RestExts], RestTypes, Acc) ->
-    acc_table_objects(Type, RestExts, RestTypes, [{Extension, Type}|Acc]);
-acc_table_objects(_Type, [], RestTypes, Acc) ->
-    acc_table_objects(RestTypes, Acc).
