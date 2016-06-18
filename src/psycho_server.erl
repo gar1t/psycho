a74cf3566a1614c5018c4dd275b6fe2082fef2a1
Garrett Smith <g@rre.tt>
2016-06-18 13:46:31 -0500

    Very remedial support for server callback module
    This facility can be used to perform init operations for a server
    process (e.g. register a server, monitor other processes, etc.)
    
    It can also be used to handle messages (e.g. sent to the process
    directly or via proc:{cast,call}.
    
    Note this is not facility for high througput interactions with a
    server - Psycho servers are still very much dedicated to handling
    inbound requests as quickly (and naively) as possible. Messages
    must wait for either a new connection or a timeout (specified by
    the accept_timeout server option). This is suitable for trivial
    message handling like stopping the server or casually swapping out
    the root app for the next request. I.e. messages are handled in
    due time, being delayed at most by the accept_timeout.

--- src/psycho_server.erl
+++ src/psycho_server.erl
@@ -7,10 +7,11 @@
 
 -export([init/1, handle_msg/3]).
 
--record(state, {handler_sup, lsock, app}).
+-record(state, {handler_sup, lsock, app, mod, accept_timeout}).
 
--define(DEFAULT_BACKLOG, 128).
--define(DEFAULT_RECBUF, 8192).
+-define(DEFAULT_BACKLOG,  128).
+-define(DEFAULT_RECBUF,   8192).
+-define(DEFAULT_ACCEPT_TIMEOUT_FOR_CB, 500).
 
 %%%===================================================================
 %%% Start / init
@@ -30,8 +31,23 @@ start_link(Binding, App, Options) ->
 
 init([Binding, App, Options]) ->
     HandlerSup = start_handler_sup(),
+    Mod = maybe_cb_init(Options),
     LSock = listen(Binding, Options),
-    {ok, init_state(HandlerSup, LSock, App), {first_msg, accept}}.
+    State = #state{
+               handler_sup=HandlerSup,
+               lsock=LSock,
+               app=App,
+               mod=Mod,
+               accept_timeout=accept_timeout_opt(Mod, Options)},
+    {ok, State, {first_msg, accept}}.
+
+maybe_cb_init(Options) ->
+    case proplists:get_value(callback, Options) of
+        undefined -> undefined;
+        {Mod, InitArg} ->
+            Mod:init(InitArg),
+            Mod
+    end.
 
 start_handler_sup() ->
     {ok, Sup} = psycho_handler_sup:start_link(),
@@ -39,26 +55,35 @@ start_handler_sup() ->
 
 listen(Binding, Options) ->
     Port = binding_port(Binding),
-    ListenOpts = listen_options(Binding, Options),
+    ListenOpts = listen_options(Options),
     handle_listen(gen_tcp:listen(Port, ListenOpts)).
 
 binding_port(Port) when is_integer(Port) -> Port;
-binding_port({_Addr, Port}) when is_integer(Port) -> Port; 
+%% TODO - use Binding to specify bound IP addr
+binding_port({_Addr, Port}) when is_integer(Port) -> Port;
 binding_port(Other) -> error({invalid_binding, Other}).
 
-listen_options(_Binding, Opts) ->
-    %% TODO - use Binding to specify bound IP addr
+listen_options(Opts) ->
     [binary,
-     {active, false},
+     {active,    false},
      {reuseaddr, true},
-     {backlog, proplists:get_value(backlog, Opts, ?DEFAULT_BACKLOG)},
-     {recbuf, proplists:get_value(recbuf, Opts, ?DEFAULT_RECBUF)}].
+     {backlog,   backlog_opt(Opts)},
+     {recbuf,    recbuf_opt(Opts)}].
 
 handle_listen({ok, LSock}) -> LSock;
 handle_listen({error, Err}) -> error({listen, Err}).
 
-init_state(HandlerSup, LSock, App) ->
-    #state{handler_sup=HandlerSup, lsock=LSock, app=App}.
+backlog_opt(Opts) ->
+    proplists:get_value(backlog, Opts, ?DEFAULT_BACKLOG).
+
+recbuf_opt(Opts) ->
+    proplists:get_value(recbuf, Opts, ?DEFAULT_RECBUF).
+
+accept_timeout_opt(undefined, _Opts) ->
+    infinity;
+accept_timeout_opt(_CbMod, Opts) ->
+    proplists:get_value(
+      accept_timeout, Opts, ?DEFAULT_ACCEPT_TIMEOUT_FOR_CB).
 
 %%%===================================================================
 %%% Message dispatch
@@ -66,13 +91,26 @@ init_state(HandlerSup, LSock, App) ->
 
 handle_msg(accept, noreply, State) ->
     handle_accept(accept(State), State),
-    {next_msg, accept, State}.
+    {next_msg, accept, State};
+handle_msg(_Msg, _From, #state{mod=undefined}=State) ->
+    {noreply, State};
+handle_msg(Msg, From, #state{app=App, mod=Mod}=State) ->
+    handle_msg_cb(Mod:handle_msg(Msg, From, App), State).
 
-accept(#state{lsock=LSock}) ->
-    gen_tcp:accept(LSock).
+handle_msg_cb({noreply, App}, State) ->
+    {noreply, State#state{app=App}};
+handle_msg_cb({reply, Reply, App}, State) ->
+    {reply, Reply, State#state{app=App}};
+handle_msg_cb({stop, Reason}, State) ->
+    {stop, Reason, State}.
+
+accept(#state{lsock=LSock, accept_timeout=Timeout}) ->
+    gen_tcp:accept(LSock, Timeout).
 
 handle_accept({ok, Sock}, State) ->
-    dispatch_request(Sock, State).
+    dispatch_request(Sock, State);
+handle_accept({error, timeout}, State) ->
+    ok.
 
 dispatch_request(Sock, #state{handler_sup=Sup, app=App}) ->
     handle_start_handler(psycho_handler_sup:start_handler(Sup, Sock, App)).
