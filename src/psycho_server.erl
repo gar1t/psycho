174cf8b9b0de9509f6f96ffcf4a3f04d9243f625
Garrett Smith <g@rre.tt>
2016-06-18 11:55:47 -0500

    Expose backlog and recbuf to options

--- src/psycho_server.erl
+++ src/psycho_server.erl
@@ -9,8 +9,8 @@
 
 -record(state, {handler_sup, lsock, app}).
 
--define(BACKLOG, 128).
--define(RECBUF, 8192).
+-define(DEFAULT_BACKLOG, 128).
+-define(DEFAULT_RECBUF, 8192).
 
 %%%===================================================================
 %%% Start / init
@@ -46,13 +46,13 @@ binding_port(Port) when is_integer(Port) -> Port;
 binding_port({_Addr, Port}) when is_integer(Port) -> Port; 
 binding_port(Other) -> error({invalid_binding, Other}).
 
-listen_options(_Binding, _Options) ->
-    %% TODO - use Binding to specify bound IP addr, expose to Options?
+listen_options(_Binding, Opts) ->
+    %% TODO - use Binding to specify bound IP addr
     [binary,
      {active, false},
      {reuseaddr, true},
-     {backlog, ?BACKLOG},
-     {recbuf, ?RECBUF}].
+     {backlog, proplists:get_value(backlog, Opts, ?DEFAULT_BACKLOG)},
+     {recbuf, proplists:get_value(recbuf, Opts, ?DEFAULT_RECBUF)}].
 
 handle_listen({ok, LSock}) -> LSock;
 handle_listen({error, Err}) -> error({listen, Err}).
